6c6719894a67b5cd27ab2eae7b093553cb42be30
Sidiel Conceição de Oliveira <sidieloliveira@ubatuba.sp.gov.br>
2024-01-17 10:12:07 -0300

    Conclusão do desafio (Simulando Uma Conta Bancária Através Do Terminal/Console)

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+import Model.DadosConta;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        DadosConta conta = new DadosConta();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        int verificaNumero = sc.nextInt();
+
+        if (verificaNumero == 1021) {
+            System.out.println(conta.toString());
+        } else {
+            System.out.println("A conta bancária não foi encontrada!");
+        }
+
+        sc.close();
     }
 }
---
+++ ContaBanco/src/Model/DadosConta.java
@@ -0,0 +1,95 @@
+package Model;
+
+public class DadosConta {
+    
+    private int numero = 1021;
+    private String agencia = "067-8";
+    private String nomeCliente = "MARIO ANDRADE";
+    private double saldo = 237.48;
+    
+    public DadosConta() {
+    }
+
+    public DadosConta(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + numero;
+        result = prime * result + ((agencia == null) ? 0 : agencia.hashCode());
+        result = prime * result + ((nomeCliente == null) ? 0 : nomeCliente.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(saldo);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DadosConta other = (DadosConta) obj;
+        if (numero != other.numero)
+            return false;
+        if (agencia == null) {
+            if (other.agencia != null)
+                return false;
+        } else if (!agencia.equals(other.agencia))
+            return false;
+        if (nomeCliente == null) {
+            if (other.nomeCliente != null)
+                return false;
+        } else if (!nomeCliente.equals(other.nomeCliente))
+            return false;
+        if (Double.doubleToLongBits(saldo) != Double.doubleToLongBits(other.saldo))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + String.valueOf(agencia) + ", conta " + String.valueOf(numero) + " e seu saldo " + String.valueOf(saldo) + " já está disponível para saque.";
+    }
+}
